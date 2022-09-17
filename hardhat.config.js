1eb16bb4e40a13b2dfc2505915e8a3adb4879d8e
sarvagnakadiya <sarvagnakadiya1@gmail.com>
2022-09-17 18:31:11 +0530

    artifacts added

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,5 +2,14 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+  solidity: '0.8.10',
+  paths: {
+    artifacts: "./src/artifacts",
+  },
+  networks: {
+    rinkeby: {
+      url: process.env.ALCHEMY_API_KEY_URL,         //Your RPC URL
+      accounts: [process.env.RINKEBY_PRIVATE_KEY],          //Your private key
+    },
+  },
 };
